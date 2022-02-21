0c2262d1b80421cdf6b1db2ce7311eea98ffb2cc
Fábio Enosse Condo <54272612+Fabio-Condo@users.noreply.github.com>
2022-02-21 16:06:45 +0200

    Update ArticleNotFoundException.java

--- src/main/java/com/example/backEndChallengeCoodesh/exception/ArticleNotFoundException.java
+++ src/main/java/com/example/backEndChallengeCoodesh/exception/ArticleNotFoundException.java
@@ -2,7 +2,7 @@ package com.example.backEndChallengeCoodesh.exception;
 
 public class ArticleNotFoundException extends Exception {
 
-	//private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 	public ArticleNotFoundException(String message) {
 		super(message);
